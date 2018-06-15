4ab0992bb29982e68b81f1418ccf7e79385df613
Adam Britt <j.adam.britt@gmail.com>
2018-06-15 16:53:31 -0400

    Completed Week 3 and mostly done with Week 4

---
+++ Week03/Wk03_HW01.py
@@ -0,0 +1,4 @@
+# Week 03 Assignment 01
+# Adam Britt
+# 6/15/2018
+print("hello world")
---
+++ Week04/Wk04_HW02.py
@@ -0,0 +1,13 @@
+# Week 04 Assignment 02
+# Adam Britt
+# 6/15/2018
+#
+# Write a program that uses input to prompt a user for their name and then
+# welcomes them.
+#
+# Desired Output = "Hello Sarah"
+#
+################################################################################
+
+name = input("Enter your name, please.")
+print("Hello", name)
