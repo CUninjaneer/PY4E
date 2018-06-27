32cf0bf620016f3074a6b054326c3f4db634e10e
Adam Britt <j.adam.britt@gmail.com>
2018-06-27 12:27:54 -0400

    Finished week 6. Finished course.

--- 2_PY_Data_Structures/Week06/Wk06_HW02.py
+++ 2_PY_Data_Structures/Week06/Wk06_HW02.py
@@ -1,28 +1,42 @@
-# Week 05 Assignment 02
+# Week 06 Assignment 02
 # Adam Britt
-# 6/24/2018
+# 6/27/2018
 #
-# 9.4
+# 10.2
 #
 # Problem Definition:
 #
-# Write a program to read through the mbox-short.txt and figure out who has the
-# sent the greatest number of mail messages. The program looks for 'From '
-# lines and takes the second word of those lines as the person who sent the
-# mail. The program creates a Python dictionary that maps the sender's mail
-# address to a count of the number of times they appear in the file. After the
-# dictionary is produced, the program reads through the dictionary using a
-# maximum loop to find the most prolific committer.
+# Write a program to read through the mbox-short.txt and figure out the
+# distribution by hour of the day for each of the messages. You can pull the
+# hour out from the 'From ' line by finding the time and then splitting the
+# string a second time using a colon.
+#
+#   From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+#
+# Once you have accumulated the counts for each hour, print out the counts,
+# sorted by hour as shown below.
 #
 #
 # Desired Output:
 #
-#   cwen@iupui.edu 5
+#   04 3
+#   06 1
+#   07 1
+#   09 2
+#   10 3
+#   11 6
+#   14 1
+#   15 2
+#   16 4
+#   17 2
+#   18 1
+#   19 1
 #
 #
 ################################################################################
 
 fname = input("Enter file name: ")
+if len(fname) < 1: fname = 'mbox-short.txt'
 # avoid tracebacks from bad filenames
 try:
     fhand = open(fname)
@@ -31,26 +45,19 @@ except:
     quit()
 # initialize any empty list to collect words
 
-emails = list()
+times = list()
 # parse inidividual lines
 for line in fhand :
     # break lines into words for further parsing
     pieces = line.split()
     if pieces == [] or pieces[0] != 'From' : continue
-    emails.append(pieces[1]) # collect email address in list
+    hms = pieces[5].split(':')
+    hour = hms[0]
+    times.append(hour) # collect times in list
 
 # build the dictionary with emails and their frequencies
 counts = dict()
-for email in emails :
-    counts[email] = counts.get(email,0) + 1
+for time in times :
+    counts[time] = counts.get(time,0) + 1
 
-# loop through dictionary to find the word with the max frequency
-bigcount = None
-bigemail = None
-for email,count in counts.items() :
-    if bigcount is None or count > bigcount :
-        bigemail = email
-        bigcount = count
-
-# print the results
-print(bigemail, bigcount)
+for k,v in sorted(counts.items()): print(k,v)
