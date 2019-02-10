2016dbab5576b70342cffaa8290ff6a45c0e3229
Adam Britt <adambritt@Kristines-MacBook-Pro.local>
2019-02-10 16:48:18 -0500

    Finished Course03 Week05 Lectures, Quiz, and Assignment.

--- 3_Using_Python_to_Access_Web_Data/Week05/Wk05_HW02.py
+++ 3_Using_Python_to_Access_Web_Data/Week05/Wk05_HW02.py
@@ -4,33 +4,56 @@
 #
 # Problem Definition:
 #
-# 
+#   Write a program to prompt for a URL and read the XML data from that URL
+#   using urllib.  Then parse and extract the comment counts from the XML data,
+#   and compute the sum of the numbers in the file.
+#
+# Data Format and Approach:
+#
+#   The data consistsof a number of names and comment counts as follows:
+#       <comment>
+#           <name>Matthias</name>
+#           <count>97</count>
+#       </comment>
+#
+#   Look through all the <comment> tags, find the <count> values, and sum the
+#   numbers.
+#   To make the code a little simpler, you can use and XPath selector string
+#   to look through the entire tree of XML for and tag named "count" with the
+#   following line of code:
+#       counts = tree.findall('.//count')
+#   Take a look at the Python ElementTree documentation and look for the
+#   supported XPath syntax for details
+#
+#
+# Sample Execution:
+#   $ python3 solution.py
+#   Enter location: http://py4e-data.dr-chuck.net/comments_42.xml
+#   Retrieving http://py4e-data.dr-chuck.net/comments_42.xml
+#   Retrieved 4189 characters
+#   Count: 50
+#   Sum: 2...
 #
 #
 ################################################################################
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
 
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import ssl
+#url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+#url = 'http://py4e-data.dr-chuck.net/comments_105084.xml'
+url = input('Enter URL: ')
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print('Retrieved', len(data), 'characters.')
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+tree = ET.fromstring(data)
+counts = tree.findall(".//count")
+print('Count:', len(counts))
 
-# url = input('Enter - ')
-url = 'http://py4e-data.dr-chuck.net/comments_105082.html'
-html = urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, "html.parser")
 total = 0
-# Retrieve all of the span tags
-tags = soup('span')
-for tag in tags:
-    # Look at the parts of a tag
-    #print('TAG:', tag)
-    #print('class', tag.get('span', None))
-    #print('Contents:', tag.contents[0])
-    #print('Attrs:', tag.attrs)
-    total = total + int(tag.contents[0])
 
-print('Sum =', total)
+for count in counts:
+     total = total + int(count.text)
+
+print('Sum :', total)
